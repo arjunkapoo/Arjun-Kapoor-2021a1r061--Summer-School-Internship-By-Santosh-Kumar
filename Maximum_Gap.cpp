2e2ad724500ba36ef68756e17e5db1644277f72d
Arjun Kapoor <95234251+arjunkapoo@users.noreply.github.com>
2024-07-19 22:07:53 +0530

    Update Maximum_Gap.cpp

--- Maximum_Gap.cpp
+++ Maximum_Gap.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int maximumGap(vector<int>& A) {
-        if (A.size() == 1)
+    int maximumGap(vector<int>& nums) {
+        int maxi = INT_MIN;
+        if(nums.size()<2){
             return 0;
-        int minVal = *min_element(begin(A), end(A));
-        int maxVal = *max_element(begin(A), end(A));
-        int N = A.size(), gap = (maxVal - minVal + N - 2) / (N - 1);
-        vector<int> mn(N - 1, INT_MAX), mx(N - 1, INT_MIN);
-        for (int n : A) {
-            if (n == minVal || n == maxVal)
-                continue;
-            int i = (n - minVal) / gap;
-            mn[i] = min(mn[i], n);
-            mx[i] = max(mx[i], n);
         }
-        int ans = gap, prev = minVal;
-        for (int i = 0; i < N - 1; ++i) {
-            if (mn[i] == INT_MAX)
-                continue;
-            ans = max(ans, mn[i] - prev);
-            prev = mx[i];
+        sort(nums.begin(), nums.end());
+        for(int i=1;i<nums.size();i++){
+            maxi = max(maxi, nums[i]- nums[i-1]);
         }
-        return max(ans, maxVal - prev);
+        return maxi;
     }
 };
